ac73155f6a18a2c2cd1909e2123ecee87e2fb9b4
Sviridenko_Egor <egoros21sviridos@gmail.com>
2021-12-29 19:23:19 +0300

    Папка

--- to_excel.py
+++ to_excel.py
@@ -1,4 +1,5 @@
 import json
+import os.path
 
 import pandas as pd
 
@@ -10,6 +11,9 @@ def file_to_json(file_name):
 
 
 def save_all_channels(dictionary, excel_file_name):
+    if not os.path.exists("Результаты"):
+        os.mkdir("Результаты")
+
     writer = pd.ExcelWriter(excel_file_name)
     append_data_to_excel("Общий список", "Назвние", [{"message": "Сообщение", "date": "Дата"}], writer, start_row=0)
     row_number = 1
